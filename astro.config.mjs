99306c4ca5857e9f34173bea85ac83d21290526a
KMinh <daukhacminh006@gmail.com>
2023-08-17 08:05:50 +0700

    SSR fix 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
+import tailwind from '@astrojs/tailwind'
+import vercelServerless from '@astrojs/vercel/serverless'
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: 'server',
-  adapter: vercel()
-});
+  adapter: vercelServerless(),
+})
