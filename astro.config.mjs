4a40bc2e1963bf338525eed83342da8dba90ece5
KMinh <daukhacminh006@gmail.com>
2023-08-18 10:07:09 +0700

    fix attempt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
-import vercel from '@astrojs/vercel/edge'
+import vercel from '@astrojs/vercel/serverless'
 
 export default defineConfig({
   integrations: [react(), tailwind()],
-  output: 'server',
+  output: 'hybrid',
   adapter: vercel(),
 })
