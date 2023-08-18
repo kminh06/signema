4f68b89bef52eecbc243fb309f2c547229b59446
KMinh <daukhacminh006@gmail.com>
2023-08-18 10:14:14 +0700

    cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
-import vercel from '@astrojs/vercel/serverless'
+import cloudflare from '@astrojs/cloudflare'
 
+// https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: 'hybrid',
-  adapter: vercel(),
+  adapter: cloudflare(),
 })
