1141ad3979e0a2537168d3c404df81f11d42b8bb
KMinh <daukhacminh006@gmail.com>
2023-08-18 09:55:23 +0700

    Edge

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
-import vercel from '@astrojs/vercel/serverless'
+import vercel from '@astrojs/vercel/edge'
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: 'server',
