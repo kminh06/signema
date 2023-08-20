0b62a85719b65e374eb7c966c586fca4f9adf523
KMinh <daukhacminh006@gmail.com>
2023-08-20 17:10:17 +0700

    Disable img lazy loading

--- src/components/Thumbnail.jsx
+++ src/components/Thumbnail.jsx
@@ -3,29 +3,12 @@ import React, { useState } from 'react'
 export default function Thumbnail(props) {
   const [starred, setStarred] = useState(props.starred)
 
-  function handleStar(e) {
-    e.preventDefault()
-    setStarred(!starred)
-  }
-
   return (
     <a
       href={`/movie/${props.no}`}
       className='w-fit block transition-all ease-in-out hover:opacity-80 relative'
     >
-      {/* {starred === true ? (
-        <StarSolid
-          className='h6 w-6 absolute top-1 right-1 text-yellow-500'
-          onClick={handleStar}
-        />
-      ) : (
-        <StarOutline
-          className='h6 w-6 absolute top-1 right-1'
-          onClick={handleStar}
-        />
-      )} */}
       <img
-        loading='lazy'
         src={props.movie.thumbnail}
         className='h-60 w-40 object-cover rounded-lg lg:h-80 lg:w-52'
         alt={props.movie.title}
