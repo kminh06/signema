16c3f507810dca9a542beac325bea18c69f2a43f
KMinh <daukhacminh006@gmail.com>
2023-09-16 21:12:02 +0700

    https

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -10,7 +10,7 @@ export default function SearchBar() {
     e.preventDefault()
     setSearching(true)
     const response = await fetch(
-      `http://www.omdbapi.com/?apikey=${
+      `https://www.omdbapi.com/?apikey=${
         import.meta.env.PUBLIC_OMDB_API_KEY
       }&t=${search}`
     )
