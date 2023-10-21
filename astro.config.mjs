2f601473978c71b23ab9796391b55f9356600c6d
KMinh <daukhacminh006@gmail.com>
2023-10-21 09:00:22 +0700

    robots txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
-import { defineConfig } from 'astro/config'
-import react from '@astrojs/react'
-import tailwind from '@astrojs/tailwind'
-import cloudflare from '@astrojs/cloudflare'
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
+import cloudflare from '@astrojs/cloudflare';
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), robotsTxt()],
   output: 'hybrid',
-  adapter: cloudflare(),
-})
+  adapter: cloudflare()
+});
