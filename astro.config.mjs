bb75a74ebe4a1ffe0744eb02b8d19eac33dd2172
KMinh <daukhacminh006@gmail.com>
2023-10-21 09:11:16 +0700

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
-import cloudflare from '@astrojs/cloudflare';
+import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
+import tailwind from '@astrojs/tailwind'
+import cloudflare from '@astrojs/cloudflare'
+import robotsTxt from 'astro-robots-txt'
 
-import robotsTxt from "astro-robots-txt";
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), robotsTxt()],
+  integrations: [react(), tailwind(), robotsTxt(), sitemap()],
   output: 'hybrid',
-  adapter: cloudflare()
-});
+  site: 'https://signema.lol',
+  adapter: cloudflare(),
+})
